31a4de97ad09c6bd5bad217f4e179537025e8a06
Michael Peters <michaelpeterswa@icloud.com>
2023-03-01 16:20:14 -0800

    fix: add example and end

--- counter/counter.go
+++ counter/counter.go
@@ -6,15 +6,17 @@ import (
 )
 
 type Counter struct {
-	text  string
-	count int
-	mutex sync.Mutex
+	prefix string
+	suffix string
+	count  int
+	mutex  sync.Mutex
 }
 
-func NewCounter(text string) *Counter {
+func NewCounter(prefix string, suffix string) *Counter {
 	return &Counter{
-		text:  text,
-		count: 0,
+		prefix: prefix,
+		suffix: suffix,
+		count:  0,
 	}
 }
 
@@ -22,5 +24,9 @@ func (c *Counter) Inc() {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 	c.count++
-	fmt.Printf("\033[%s %d", c.text, c.count)
+	fmt.Printf("\r%s%d%s", c.prefix, c.count, c.suffix)
+}
+
+func (c *Counter) End() {
+	fmt.Println()
 }
