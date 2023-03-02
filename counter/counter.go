b2f804d66f23aec3d336bbe02ad02a930d836b6e
Michael Peters <michaelpeterswa@icloud.com>
2023-03-01 21:41:59 -0800

    fix: update with a safe default

--- counter/counter.go
+++ counter/counter.go
@@ -3,8 +3,11 @@ package counter
 import (
 	"fmt"
 	"sync"
+
+	"github.com/fatih/color"
 )
 
+// Counter is a simple counter that can be incremented, decremented, or set to a specific value.
 type Counter struct {
 	prefix string
 	suffix string
@@ -12,6 +15,12 @@ type Counter struct {
 	mutex  sync.Mutex
 }
 
+// NewDefaultCounter creates a counter with the default (and colored) prefix and suffix.
+func NewDefaultCounter(task string) *Counter {
+	return NewCounter(color.CyanString("Running %s ", task)+color.RedString("["), "x"+color.RedString("]"))
+}
+
+// NewCounter creates a counter with the given prefix and suffix.
 func NewCounter(prefix string, suffix string) *Counter {
 	return &Counter{
 		prefix: prefix,
@@ -20,6 +29,7 @@ func NewCounter(prefix string, suffix string) *Counter {
 	}
 }
 
+// Inc increments the counter by 1.
 func (c *Counter) Inc() {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
@@ -27,6 +37,23 @@ func (c *Counter) Inc() {
 	fmt.Printf("\r%s%d%s", c.prefix, c.count, c.suffix)
 }
 
+// Dec decrements the counter by 1.
+func (c *Counter) Dec() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.count--
+	fmt.Printf("\r%s%d%s", c.prefix, c.count, c.suffix)
+}
+
+// To sets the counter to the given value.
+func (c *Counter) To(n int) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.count = n
+	fmt.Printf("\r%s%d%s", c.prefix, c.count, c.suffix)
+}
+
+// End prints a newline to the console. This should be called after the counter is no longer needed.
 func (c *Counter) End() {
 	fmt.Println()
 }
